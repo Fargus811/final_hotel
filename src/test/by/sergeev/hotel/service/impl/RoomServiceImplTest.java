d4b928b325d9f7a6bc51897e94d78536caf09517
Daniil Sergeev <ggas10041997@gmail.com>
2020-12-03 21:11:40 +0300

    database

---
+++ src/test/by/sergeev/hotel/service/impl/RoomServiceImplTest.java
@@ -0,0 +1,41 @@
+package by.sergeev.hotel.service.impl;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class RoomServiceImplTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @Test
+    public void testFindAll() {
+    }
+
+    @Test
+    public void testGetRoomsByPage() {
+    }
+
+    @Test
+    public void testGetAmountOfPages() {
+    }
+
+    @Test
+    public void testFindRoomById() {
+    }
+
+    @Test
+    public void testFindFreeRoomsByBooking() {
+    }
+
+    @Test
+    public void testCreateRoom() {
+    }
+
+    @Test
+    public void testUpdateRoomInfo() {
+    }
+}
---
+++ src/test/by/sergeev/hotel/service/impl/UserServiceImplTest.java
@@ -0,0 +1,61 @@
+package by.sergeev.hotel.service.impl;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class UserServiceImplTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @Test
+    public void testFindAllUsers() {
+    }
+
+    @Test
+    public void testCheckIsValid() {
+    }
+
+    @Test
+    public void testCheckIsEmailFree() {
+    }
+
+    @Test
+    public void testRegister() {
+    }
+
+    @Test
+    public void testLogIn() {
+    }
+
+    @Test
+    public void testAddBalance() {
+    }
+
+    @Test
+    public void testFindUserById() {
+    }
+
+    @Test
+    public void testUpdateUser() {
+    }
+
+    @Test
+    public void testUpdateUserPassword() {
+    }
+
+    @Test
+    public void testDeleteAccount() {
+    }
+
+    @Test
+    public void testChangeAccountStatus() {
+    }
+
+    @Test
+    public void testActivateAccount() {
+    }
+}
