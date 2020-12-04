a484e459d5328a7c7c9dc534b9161a815416ceb2
Daniil Sergeev <ggas10041997@gmail.com>
2020-12-04 13:01:14 +0300

    order filters

--- src/test/by/sergeev/hotel/service/impl/BookingServiceImplTest.java
+++ src/test/by/sergeev/hotel/service/impl/BookingServiceImplTest.java
@@ -1,22 +1,34 @@
 package by.sergeev.hotel.service.impl;
 
+import by.sergeev.hotel.dao.BookingDao;
 import by.sergeev.hotel.dao.DaoFactory;
 import by.sergeev.hotel.dao.impl.BookingDaoImpl;
+import by.sergeev.hotel.entity.Booking;
+import by.sergeev.hotel.exception.DaoException;
+import by.sergeev.hotel.exception.ServiceException;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.testng.PowerMockObjectFactory;
+import org.powermock.reflect.Whitebox;
 import org.testng.IObjectFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.ObjectFactory;
 import org.testng.annotations.Test;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.*;
 
 @PowerMockIgnore({"com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*", "org.w3c.*",
         "com.sun.org.apache.xalan.*", "javax.management.*"})
 @PrepareForTest({BookingDaoImpl.class})
 public class BookingServiceImplTest {
 
-    BookingDaoImpl bookingDao;
+    BookingDao bookingDao;
+    BookingServiceImpl bookingService;
 
     @ObjectFactory
     public IObjectFactory setObjectFactory() {
@@ -25,12 +37,21 @@ public class BookingServiceImplTest {
 
     @BeforeMethod
     public void setUp() {
-        bookingDao = Mockito.mock(BookingDaoImpl.class);
- //       Mockito.when(DaoFactory.daoFactory).thenReturn(bookingDao);
+        bookingDao = Mockito.mock(BookingDao.class);
+        bookingService = new BookingServiceImpl();
+
     }
 
     @Test
     public void testFindBookingById() {
+        try {
+            Optional<Booking> expected = Optional.of(new Booking());
+            when(bookingDao.findBookingById(any(Long.class))).thenReturn(expected);
+            Optional<Booking> actual = bookingService.findBookingById(1);
+            assertEquals(actual, expected);
+        } catch (ServiceException | DaoException e) {
+            fail("Incorrect data", e);
+        }
     }
 
     @Test
